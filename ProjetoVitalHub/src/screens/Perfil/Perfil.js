afab976573b46f4c02747afbf6c149fae93451fa
Matheus <matheusenrikeramalho@gmail.com>
2024-04-09 17:25:06 -0300

    Validacao do atualizar perfil feita

--- ProjetoVitalHub/src/screens/Perfil/Perfil.js
+++ ProjetoVitalHub/src/screens/Perfil/Perfil.js
@@ -18,90 +18,81 @@ import api from "../../services/services"
 import { faL } from "@fortawesome/free-solid-svg-icons"
 import { Spinner } from "../../components/Spinner"
 
-// Array de objetos mocados
-let profile =
-{
-    id: 1,
-    nome: 'Pedro Felix',
-    email: 'felixpedro@gmail',
-    nascimento: '25/02/2004',
-    cpf: '010.101.010/10',
-    logradouro: 'Rua da minha casa',
-    cep: '00000-000',
-    cidade: 'Itaquaquecetuba'
-}
-
-
 
 export const Perfil = ({
     navigation,
 }) => {
 
+    // DADOS DO USUARIO
     const [userId, setUserId] = useState();
     const [userName, setUserName] = useState();
+    const [userRole, setUserRole] = useState();
     const [userEmail, setUserEmail] = useState();
+    const [token, setToken] = useState();
+
+    // DADOS DO PACIENTE
     const [dataNascimento, setDataNascimento] = useState();
     const [cpf, setCpf] = useState();
+    const [rg, setRg] = useState();
     const [cep, setCep] = useState();
     const [logradouro, setLogradouro] = useState();
     const [cidade, setCidade] = useState();
-
-    const [token, setToken] = useState();
     const [perfil, setPerfil] = useState();
 
+    // DADOS DO MÉDICO
+    const [crm, setCrm] = useState();
+
+    // SPINNER
     const [spinner, setSpinner] = useState();
 
     // Ativa a edicao do perfil
     const [edit, setEdit] = useState(false);
 
+
+    // CARREGA O USUARIO
     async function profileLoad() {
         const token = await userDecodeToken();
 
         if (token) {
             setUserName(token.name)
             setUserEmail(token.email)
+            setUserRole(token.role)
             setUserId(token.id)
+            setToken(token)
 
             getPerfil(token)
 
-            // setToken(token)
         }
     }
 
-    async function getPerfil(user) {
+    // BUSCAR O USUARIO
+    async function getPerfil(token) {
 
-        let rota = '';
+        const rota = (token.role === 'Paciente' ? `Pacientes` : `Medicos`);
 
-        if (token.role === 'Paciente') rota = `Pacientes`
-        if (token.role === 'Medico') rota = `Medicos`
-
-        const promise = await api.get(`/${rota}/BuscarPorId?id=${user.id}`);
+        const promise = await api.get(`/${rota}/BuscarPorId?id=${token.id}`);
         const response = promise.data
 
-        setDataNascimento(response.dataNascimento);
-        setCpf(response.cpf)
-        setLogradouro(response.endereco.logradouro)
-        setCep(response.endereco.cep)
-        setCidade(response.endereco.cidade)
+        setPerfil(response)
 
-
-        // else (token.role === 'Medico')
-        // {
-        //     const promise = await api.get(`/Medicos/BuscarPorId?id=${userId}`);
-        //     setPerfil(promise.data)
-
-        //     setDataNascimento(perfil.dataNascimento);
-        //     setCpf(perfil.cpf)
-        //     setLogradouro(perfil.endereco.logradouro)
-        //     setCep(perfil.endereco.cep)
-        //     setCidade(perfil.endereco.cidade)
-        // }
-
+        if (rota === 'Medicos') {
+            setCrm(response.crm)
+        }
+        else {
+            setDataNascimento(response.dataNascimento)
+            setLogradouro(response.endereco.logradouro)
+            setCep(response.endereco.cep)
+            setCidade(response.endereco.cidade)
+            setCpf(response.cpf)
+            setRg(response.rg)
+        }
     }
 
+    // ATUALIZAR O USUARIO
     async function putPerfil() {
         await api.put(`/Pacientes/AtualizarPerfil?idUsuario=${userId}`, {
             "cpf": cpf,
+            "rg": rg,
             "dataNascimento": dataNascimento,
             "cep": cep,
             "logradouro": logradouro,
@@ -111,9 +102,7 @@ export const Perfil = ({
 
 
     useEffect(() => {
-        // console.log('Use efecct');
         profileLoad()
-        // getPerfil()
     }, [])
 
     return (
@@ -132,82 +121,106 @@ export const Perfil = ({
                 <FotoStyle source={fotoPerfil} />
 
                 <DadosPessoais>
-                    <Title textAlign={"center"}>{userName}</Title>
-                    <Subtitle>{userEmail}</Subtitle>
+                    <Title textAlign={"center"}>{userName !== null ? userName : null}</Title>
+                    <Subtitle>{userEmail !== null ? userEmail : null}</Subtitle>
                 </DadosPessoais>
 
-                <ContainerPerfil>
-                    <BoxInput
-                        onChangeText={(x) => setDataNascimento(x)}
-                        textLabel='Data de nascimento'
-                        placeholder={
-                            !edit
-                                ? new Date(dataNascimento).toLocaleDateString()
-                                : new Date(perfil.dataNascimento).toLocaleDateString()
-                        }
-                        // fieldValue={!edit ? dataNascimento : null}
-                        editable={edit}
-                    />
-                    <BoxInput
-                        onChangeText={(x) => setCpf(x)}
-                        textLabel='CPF'
-                        placeholder={!edit ? cpf : perfil.cpf}
-                        // fieldValue={!edit ? cpf : ''}
-                        keyType={"numeric"}
-                        editable={edit}
-                    />
-                    <BoxInput
-                        onChangeText={(x) => setLogradouro(x)}
-                        textLabel='Endereço'
-                        placeholder={!edit ? logradouro : perfil.endereco.logradouro}
-                        // fieldValue={!edit ? logradouro : ''}
-                        editable={edit}
-                    />
-                    <ContainerBox>
-                        <BoxInput
-                            onChangeText={(x) => setCep(x)}
-                            fieldWidth={45}
-                            textLabel='CEP'
-                            placeholder={!edit ? cep : perfil.endereco.cep}
-                            // fieldValue={!edit ? cep : ''}
-                            editable={edit}
-                        />
-
-                        <BoxInput
-                            onChangeText={(x) => setCidade(x)}
-                            fieldWidth={50}
-                            textLabel='Cidade'
-                            placeholder={!edit ? cidade : perfil.endereco.cidade}
-                            // fieldValue={!edit ? cidade : ''}
-                            editable={edit}
-                        />
-                    </ContainerBox>
-
-                    <ContainerInputButtom>
-                        <Button
-                            style={!edit ? { backgroundColor: '#ACABB7', borderColor: '#ACABB7' } : null}
-                            onPress={() => {
-                                setEdit(false)
-                                putPerfil()
-                                setSpinner(true)
-                            }}
-                            disabled={!edit}
+                {
+                    userRole === 'Paciente' ?
+                        <ContainerPerfil
+                            style={{ marginBottom: 40 }}
                         >
-                            <ButtonTitle>Salvar</ButtonTitle>
-                        </Button>
+                            <BoxInput
+                                onChangeText={(x) => setDataNascimento(x)}
+                                textLabel='Data de nascimento'
+                                placeholder={
+                                    !edit ? new Date(dataNascimento).toLocaleDateString()
+                                        : new Date(perfil.dataNascimento).toLocaleDateString()
+                                }
+                                editable={edit}
+                            />
+                            <BoxInput
+                                onChangeText={(x) => setCpf(x)}
+                                textLabel='CPF'
+                                placeholder={
+                                    !edit ? cpf
+                                        : perfil.cpf
+                                }
+                                keyType={"numeric"}
+                                editable={edit}
+                            />
+                            <BoxInput
+                                onChangeText={(x) => setRg(x)}
+                                textLabel='RG'
+                                placeholder={
+                                    !edit ? rg
+                                        : perfil.rg
+                                }
+                                keyType={"numeric"}
+                                editable={edit}
+                            />
+                            <BoxInput
+                                onChangeText={(x) => setLogradouro(x)}
+                                textLabel='Endereço'
+                                placeholder={!edit ? logradouro : perfil.endereco.logradouro}
+                                editable={edit}
+                            />
+                            <ContainerBox>
+                                <BoxInput
+                                    onChangeText={(x) => setCep(x)}
+                                    fieldWidth={45}
+                                    textLabel='CEP'
+                                    placeholder={!edit ? cep : perfil.endereco.cep}
+                                    editable={edit}
+                                />
 
-                        <Button
-                            onPress={() => setEdit(true)}
-                            style={edit ? { backgroundColor: '#ACABB7', borderColor: '#ACABB7' } : null}
+                                <BoxInput
+                                    onChangeText={(x) => setCidade(x)}
+                                    fieldWidth={50}
+                                    textLabel='Cidade'
+                                    placeholder={!edit ? cidade : perfil.endereco.cidade}
+                                    editable={edit}
+                                />
+                            </ContainerBox>
+                        </ContainerPerfil>
+                        :
+                        <ContainerPerfil
+                            style={{ marginBottom: 40 }}
                         >
-                            <ButtonTitle>editar</ButtonTitle>
-                        </Button>
+                            <BoxInput
+                                onChangeText={(x) => setDataNascimento(x)}
+                                textLabel='CRM'
+                                placeholder={crm}
+                                editable={edit}
+                            />
+                        </ContainerPerfil>
+                }
 
-                        {/* <Button fieldBckColor={"#ACABB7"} fieldBorderColor={"#ACABB7"}>
+                <ContainerInputButtom>
+                    <Button
+                        style={!edit ? { backgroundColor: '#ACABB7', borderColor: '#ACABB7' } : null}
+                        onPress={() => {
+                            setEdit(false)
+                            putPerfil()
+                            setSpinner(true)
+                        }}
+                        disabled={!edit}
+                    >
+                        <ButtonTitle>Salvar</ButtonTitle>
+                    </Button>
+
+                    <Button
+                        onPress={() => setEdit(true)}
+                        style={edit ? { backgroundColor: '#ACABB7', borderColor: '#ACABB7' } : null}
+                    >
+                        <ButtonTitle>editar</ButtonTitle>
+                    </Button>
+
+                    {/* <Button fieldBckColor={"#ACABB7"} fieldBorderColor={"#ACABB7"}>
                             <ButtonTitle>sair do app</ButtonTitle>
                         </Button> */}
-                    </ContainerInputButtom>
-                </ContainerPerfil>
+                </ContainerInputButtom>
+
             </Scroll>
         </Container>
     )
--- WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a55c6e86d739ac1524d9d740443b86daff8fa8f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11b9a1f33425fca4fd8e0ea9ab6a112df6efc5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
