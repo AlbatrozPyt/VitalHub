1ccc9df5996d5f8a2658b2887b6f03ca3aac9f2d
PedroFelix40 <48746019871@info.local>
2024-04-16 14:24:03 -0300

    Feito os serviços de email, upload de imagens e OCR

--- WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d641413059aa1954585407a74b6e47eb0f01b93b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0745c26d4b2fb350dedcf77b76cb107892297ce5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
